a0bd9e4e797c588c509f86dae252457593737a25
Vinny1109 <104118028+Vinny1109@users.noreply.github.com>
2023-11-23 23:21:49 +0700

    Add files via upload

---
+++ laundry/Price/index.php
@@ -0,0 +1,64 @@
+
+<link href="css/Price-style.css" rel="stylesheet" type="text/css" media="all" />
+
+
+
+<div class="main">
+	<div class="padding-w3l">
+		<div class="content">
+			<div class="grid1">
+				<div class="header">
+					<h3>Men</h3>
+				</div>
+				<div class="info-agile">
+					<p> Free Pick_up</p>
+					<p> Total Clothes</p>
+					<p class="text"><span>	24/7</span> Support</p>
+					<p> Free Delivery</p>
+				</div>
+				<div class="submit">
+					<a class=" sign-up popup-with-zoom-anim"  data-toggle="modal" data-target="#myModal1">Order</a>
+				</div>
+			</div>
+			
+			
+			<div class="grid2">
+				<div class="header">
+					<h3>Women</h3>
+				</div>
+				<div class="info-agile">
+					<p> Free Pick_up</p>
+					<p> Total Clothes</p>
+					<p class="text"><span>	24/7</span> Support</p>
+					<p> Free Delivery</p>
+	            </div>
+				<div class="submit">
+					<a class=" sign-up popup-with-zoom-anim"  data-toggle="modal" data-target="#myModal1">Order</a>
+				</div>
+			</div>
+			
+			<div class="grid3">
+				<div class="header">
+					<h3>Children</h3>
+				</div>
+				<div class="info-agile">
+					<p> Free Pick_up</p>
+					<p> Total Clothes</p>
+					<p class="text"><span>	24/7</span> Support</p>
+					<p> Free Delivery</p>
+				</div>
+				<div class="submit">
+					<a class=" sign-up popup-with-zoom-anim"  data-toggle="modal" data-target="#myModal1">Order</a>
+				</div>
+			</div>
+
+			
+		
+		  <div class="clear"> </div>
+		</div>
+		
+		
+
+		
+	</div>
+</div>
